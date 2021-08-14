ce45cb788b4c06462d6d2b15c0e8ebb2cbea86ee
karineB <karinebondarenko03@gmail.com>
2021-08-14 17:28:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+const TelegramApi = require("node-telegram-bot-api");
+const { gameOptions, againOptions } = require("./options");
+
+const token = "1942269205:AAGw7fX3gDGv7IPkHO04rRB0gzTlJLkYoAE";
+const bot = new TelegramApi(token, { polling: true });
+
+const startGame = async (chatId) => {
+  await bot.sendMessage(
+    chatId,
+    `Сейчас я загадаю цифру от 0 до 9, а ты должен ее угадать!`
+  );
+  const randomNumber = Math.floor(Math.random() * 10);
+  chats[chatId] = randomNumber;
+  await bot.sendMessage(chatId, "Отгадывай", gameOptions);
+};
+
+bot.setMyCommands([
+  { command: "/start", description: "Начальное приветствие" },
+  { command: "/info", description: "Получить информацию о пользователе" },
+  { command: "/game", description: "Игра угадай цифру" },
+]);
+
+const start = () => {
+  bot.on("message", async (msg) => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+    // bot.sendMessage(chatId, `lorem ${text}`);
+
+    if (text === "/start") {
+      await bot.sendSticker(
+        chatId,
+        "https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp"
+      );
+      return bot.sendMessage(chatId, `Добро пожаловать в телеграм бот`);
+    }
+    if (text === "/info") {
+      return bot.sendMessage(
+        chatId,
+        `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`
+      );
+    }
+    if (text === "/game") {
+      return startGame(chatId);
+    }
+    return bot.sendMessage(chatId, "Я тебя не понимаю, попробуй еще раз!)");
+  });
+
+  bot.on("callback_query", async (msg) => {
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+    if (data === "/again") {
+      return startGame(chatId);
+    }
+    const user = await UserModel.findOne({ chatId });
+    if (data == chats[chatId]) {
+      user.right += 1;
+      await bot.sendMessage(
+        chatId,
+        `Поздравляю, ты отгадал цифру ${chats[chatId]}`,
+        againOptions
+      );
+    } else {
+      user.wrong += 1;
+      await bot.sendMessage(
+        chatId,
+        `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`,
+        againOptions
+      );
+    }
+    await user.save();
+  });
+};
+
+start();
